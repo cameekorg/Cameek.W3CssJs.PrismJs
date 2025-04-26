8434c393dced77918e02edc5c9e1693f0360c5b5
jarosm3@gmail.com <jarosm3@gmail.com>
2025-04-26 23:15:57 +0200

    Add syntax highlighting for multiple languages in Prism.js
    - Introduced C++ syntax highlighting with keywords, operators, and class names.
    - Added CSS language support including comments, at-rules, and properties.
    - Implemented Kotlin syntax highlighting with keywords, functions, and string literals.
    - Integrated PHP syntax highlighting with support for variables, functions, and comments.
    - Added PowerShell language support with keywords, comments, and string handling.
    - Introduced properties language support for key-value pairs.
    - Implemented Python syntax highlighting with support for comments, strings, and functions.
    - Added Ruby syntax highlighting with support for comments, class names, and methods.
    - Integrated SQL syntax highlighting with keywords, comments, and string handling.
    - Introduced YAML syntax highlighting with support for scalars, comments, and key-value pairs.

--- .scripts/download-prismjs.bat
+++ .scripts/download-prismjs.bat
@@ -21,15 +21,38 @@ set URL_PLUGIN_LINE_NUMBERS_JS=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/pl
 set URL_PLUGIN_LINE_NUMBERS_CSS=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/plugins/line-numbers/prism-line-numbers.min.css
 
 :: Languages
+set URL_LANG_BATCH=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-batch.min.js
+set URL_LANG_C=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-c.min.js
+set URL_LANG_CPP=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-cpp.min.js
 set URL_LANG_CSHARP=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-csharp.min.js
+set URL_LANG_CSS=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-css.min.js
+set URL_LANG_HTML=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-html.min.js
 set URL_LANG_JAVA=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-java.min.js
-set URL_LANG_JSON=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-json.min.js
-set URL_LANG_XML=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-xml-doc.min.js
 set URL_LANG_JAVASCRIPT=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-javascript.min.js
+set URL_LANG_JSON=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-json.min.js
+set URL_LANG_KOTLIN=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-kotlin.min.js
+set URL_LANG_PHP=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-php.min.js
+set URL_LANG_POWERSHELL=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-powershell.min.js
+set URL_LANG_PROPERTIES=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-properties.min.js
+set URL_LANG_PYTHON=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-python.min.js
+set URL_LANG_RUBY=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-ruby.min.js
 set URL_LANG_SHELL=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-shell-session.min.js
+set URL_LANG_SQL=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-sql.min.js
+set URL_LANG_XML=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-xml-doc.min.js
+set URL_LANG_YAML=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/components/prism-yaml.min.js
 
 :: License
 set URL_LICENSE=https://raw.githubusercontent.com/PrismJS/prism/v%VERSION%/LICENSE
+
+:: Add URLs for all themes
+set URL_THEME_DEFAULT=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/themes/prism.min.css
+set URL_THEME_COY=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/themes/prism-coy.min.css
+set URL_THEME_DARK=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/themes/prism-dark.min.css
+set URL_THEME_FUNKY=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/themes/prism-funky.min.css
+set URL_THEME_OKAIDIA=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/themes/prism-okaidia.min.css
+set URL_THEME_SOLARIZED_LIGHT=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/themes/prism-solarizedlight.min.css
+set URL_THEME_TOMORROW=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/themes/prism-tomorrow.min.css
+set URL_THEME_TWILIGHT=https://cdn.jsdelivr.net/npm/prismjs@%VERSION%/themes/prism-twilight.min.css
 
 @echo.
 @echo.
@@ -46,14 +69,38 @@ set TARGET_CSS_FILE=%TARGET_CSS_DIR%/prism.min.css
 set TARGET_PLUGIN_LINE_NUMBERS_JS=%TARGET_JS_DIR%/prism-line-numbers.min.js
 set TARGET_PLUGIN_LINE_NUMBERS_CSS=%TARGET_CSS_DIR%/prism-line-numbers.min.css
 
+set TARGET_LANG_BATCH=%TARGET_JS_DIR%/prism-batch.min.js
+set TARGET_LANG_C=%TARGET_JS_DIR%/prism-c.min.js
+set TARGET_LANG_CPP=%TARGET_JS_DIR%/prism-cpp.min.js
 set TARGET_LANG_CSHARP=%TARGET_JS_DIR%/prism-csharp.min.js
+set TARGET_LANG_CSS=%TARGET_JS_DIR%/prism-css.min.js
+set TARGET_LANG_HTML=%TARGET_JS_DIR%/prism-html.min.js
 set TARGET_LANG_JAVA=%TARGET_JS_DIR%/prism-java.min.js
-set TARGET_LANG_JSON=%TARGET_JS_DIR%/prism-json.min.js
-set TARGET_LANG_XML=%TARGET_JS_DIR%/prism-xml-doc.min.js
 set TARGET_LANG_JAVASCRIPT=%TARGET_JS_DIR%/prism-javascript.min.js
+set TARGET_LANG_JSON=%TARGET_JS_DIR%/prism-json.min.js
+set TARGET_LANG_KOTLIN=%TARGET_JS_DIR%/prism-kotlin.min.js
+set TARGET_LANG_PHP=%TARGET_JS_DIR%/prism-php.min.js
+set TARGET_LANG_POWERSHELL=%TARGET_JS_DIR%/prism-powershell.min.js
+set TARGET_LANG_PROPERTIES=%TARGET_JS_DIR%/prism-properties.min.js
+set TARGET_LANG_PYTHON=%TARGET_JS_DIR%/prism-python.min.js
+set TARGET_LANG_RUBY=%TARGET_JS_DIR%/prism-ruby.min.js
 set TARGET_LANG_SHELL=%TARGET_JS_DIR%/prism-shell-session.min.js
+set TARGET_LANG_SQL=%TARGET_JS_DIR%/prism-sql.min.js
+set TARGET_LANG_XML=%TARGET_JS_DIR%/prism-xml-doc.min.js
+set TARGET_LANG_YAML=%TARGET_JS_DIR%/prism-yaml.min.js
 
 set TARGET_LICENSE=%TARGET_WWW_DIR%/LICENSE-prismjs.txt
+
+:: Add target file paths for all themes
+set TARGET_THEME_DEFAULT=%TARGET_CSS_DIR%/prism.min.css
+set TARGET_THEME_COY=%TARGET_CSS_DIR%/prism-coy.min.css
+set TARGET_THEME_DARK=%TARGET_CSS_DIR%/prism-dark.min.css
+set TARGET_THEME_FUNKY=%TARGET_CSS_DIR%/prism-funky.min.css
+set TARGET_THEME_OKAIDIA=%TARGET_CSS_DIR%/prism-okaidia.min.css
+set TARGET_THEME_SOLARIZED_LIGHT=%TARGET_CSS_DIR%/prism-solarizedlight.min.css
+set TARGET_THEME_TOMORROW=%TARGET_CSS_DIR%/prism-tomorrow.min.css
+set TARGET_THEME_TWILIGHT=%TARGET_CSS_DIR%/prism-twilight.min.css
+
 @echo.
 @echo.
 
@@ -74,15 +121,39 @@ curl -o %TARGET_PLUGIN_LINE_NUMBERS_JS% %URL_PLUGIN_LINE_NUMBERS_JS%
 curl -o %TARGET_PLUGIN_LINE_NUMBERS_CSS% %URL_PLUGIN_LINE_NUMBERS_CSS%
 
 :: Languages
+curl -o %TARGET_LANG_BATCH% %URL_LANG_BATCH%
+curl -o %TARGET_LANG_C% %URL_LANG_C%
+curl -o %TARGET_LANG_CPP% %URL_LANG_CPP%
 curl -o %TARGET_LANG_CSHARP% %URL_LANG_CSHARP%
+curl -o %TARGET_LANG_CSS% %URL_LANG_CSS%
+curl -o %TARGET_LANG_HTML% %URL_LANG_HTML%
 curl -o %TARGET_LANG_JAVA% %URL_LANG_JAVA%
-curl -o %TARGET_LANG_JSON% %URL_LANG_JSON%
-curl -o %TARGET_LANG_XML% %URL_LANG_XML%
 curl -o %TARGET_LANG_JAVASCRIPT% %URL_LANG_JAVASCRIPT%
+curl -o %TARGET_LANG_JSON% %URL_LANG_JSON%
+curl -o %TARGET_LANG_KOTLIN% %URL_LANG_KOTLIN%
+curl -o %TARGET_LANG_PHP% %URL_LANG_PHP%
+curl -o %TARGET_LANG_POWERSHELL% %URL_LANG_POWERSHELL%
+curl -o %TARGET_LANG_PROPERTIES% %URL_LANG_PROPERTIES%
+curl -o %TARGET_LANG_PYTHON% %URL_LANG_PYTHON%
+curl -o %TARGET_LANG_RUBY% %URL_LANG_RUBY%
 curl -o %TARGET_LANG_SHELL% %URL_LANG_SHELL%
+curl -o %TARGET_LANG_SQL% %URL_LANG_SQL%
+curl -o %TARGET_LANG_XML% %URL_LANG_XML%
+curl -o %TARGET_LANG_YAML% %URL_LANG_YAML%
 
 :: License
 curl -o %TARGET_LICENSE% %URL_LICENSE%
+
+:: Download all themes
+curl -o %TARGET_THEME_DEFAULT% %URL_THEME_DEFAULT%
+curl -o %TARGET_THEME_COY% %URL_THEME_COY%
+curl -o %TARGET_THEME_DARK% %URL_THEME_DARK%
+curl -o %TARGET_THEME_FUNKY% %URL_THEME_FUNKY%
+curl -o %TARGET_THEME_OKAIDIA% %URL_THEME_OKAIDIA%
+curl -o %TARGET_THEME_SOLARIZED_LIGHT% %URL_THEME_SOLARIZED_LIGHT%
+curl -o %TARGET_THEME_TOMORROW% %URL_THEME_TOMORROW%
+curl -o %TARGET_THEME_TWILIGHT% %URL_THEME_TWILIGHT%
+
 @echo.
 @echo.
 
--- PathsPrismJs.cs
+++ PathsPrismJs.cs
@@ -54,4 +54,106 @@ public class PathsPrismJs
     /// <code>_content/Cameek.W3CssJs.PrismJs/js/prism-shell-session.min.js</code>
     /// </summary>
     public static string PrismShellSessionMinJs => "_content/Cameek.W3CssJs.PrismJs/js/prism-shell-session.min.js";
+
+    /// <summary>
+    /// PrismJS language file for Batch syntax
+    /// <code>_content/Cameek.W3CssJs.PrismJs/js/prism-batch.min.js</code>
+    /// </summary>
+    public static string PrismBatchMinJs => "_content/Cameek.W3CssJs.PrismJs/js/prism-batch.min.js";
+
+    /// <summary>
+    /// PrismJS language file for C syntax
+    /// <code>_content/Cameek.W3CssJs.PrismJs/js/prism-c.min.js</code>
+    /// </summary>
+    public static string PrismCMinJs => "_content/Cameek.W3CssJs.PrismJs/js/prism-c.min.js";
+
+    /// <summary>
+    /// PrismJS language file for C++ syntax
+    /// <code>_content/Cameek.W3CssJs.PrismJs/js/prism-cpp.min.js</code>
+    /// </summary>
+    public static string PrismCppMinJs => "_content/Cameek.W3CssJs.PrismJs/js/prism-cpp.min.js";
+
+    /// <summary>
+    /// PrismJS language file for Kotlin syntax
+    /// <code>_content/Cameek.W3CssJs.PrismJs/js/prism-kotlin.min.js</code>
+    /// </summary>
+    public static string PrismKotlinMinJs => "_content/Cameek.W3CssJs.PrismJs/js/prism-kotlin.min.js";
+
+    /// <summary>
+    /// PrismJS language file for Python syntax
+    /// <code>_content/Cameek.W3CssJs.PrismJs/js/prism-python.min.js</code>
+    /// </summary>
+    public static string PrismPythonMinJs => "_content/Cameek.W3CssJs.PrismJs/js/prism-python.min.js";
+
+    /// <summary>
+    /// PrismJS language file for Ruby syntax
+    /// <code>_content/Cameek.W3CssJs.PrismJs/js/prism-ruby.min.js</code>
+    /// </summary>
+    public static string PrismRubyMinJs => "_content/Cameek.W3CssJs.PrismJs/js/prism-ruby.min.js";
+
+    /// <summary>
+    /// PrismJS language file for SQL syntax
+    /// <code>_content/Cameek.W3CssJs.PrismJs/js/prism-sql.min.js</code>
+    /// </summary>
+    public static string PrismSqlMinJs => "_content/Cameek.W3CssJs.PrismJs/js/prism-sql.min.js";
+
+    /// <summary>
+    /// PrismJS language file for YAML syntax
+    /// <code>_content/Cameek.W3CssJs.PrismJs/js/prism-yaml.min.js</code>
+    /// </summary>
+    public static string PrismYamlMinJs => "_content/Cameek.W3CssJs.PrismJs/js/prism-yaml.min.js";
+
+    /// <summary>
+    /// PrismJS theme file for the default theme
+    /// <code>_content/Cameek.W3CssJs.PrismJs/css/prism.min.css</code>
+    /// </summary>
+    public static string PrismThemeDefaultMinCss => "_content/Cameek.W3CssJs.PrismJs/css/prism.min.css";
+
+    /// <summary>
+    /// PrismJS theme file for the Coy theme
+    /// <code>_content/Cameek.W3CssJs.PrismJs/css/prism-coy.min.css</code>
+    /// </summary>
+    public static string PrismThemeCoyMinCss => "_content/Cameek.W3CssJs.PrismJs/css/prism-coy.min.css";
+
+    /// <summary>
+    /// PrismJS theme file for the Dark theme
+    /// <code>_content/Cameek.W3CssJs.PrismJs/css/prism-dark.min.css</code>
+    /// </summary>
+    public static string PrismThemeDarkMinCss => "_content/Cameek.W3CssJs.PrismJs/css/prism-dark.min.css";
+
+    /// <summary>
+    /// PrismJS theme file for the Funky theme
+    /// <code>_content/Cameek.W3CssJs.PrismJs/css/prism-funky.min.css</code>
+    /// </summary>
+    public static string PrismThemeFunkyMinCss => "_content/Cameek.W3CssJs.PrismJs/css/prism-funky.min.css";
+
+    /// <summary>
+    /// PrismJS theme file for the Okaidia theme
+    /// <code>_content/Cameek.W3CssJs.PrismJs/css/prism-okaidia.min.css</code>
+    /// </summary>
+    public static string PrismThemeOkaidiaMinCss => "_content/Cameek.W3CssJs.PrismJs/css/prism-okaidia.min.css";
+
+    /// <summary>
+    /// PrismJS theme file for the Solarized Light theme
+    /// <code>_content/Cameek.W3CssJs.PrismJs/css/prism-solarizedlight.min.css</code>
+    /// </summary>
+    public static string PrismThemeSolarizedLightMinCss => "_content/Cameek.W3CssJs.PrismJs/css/prism-solarizedlight.min.css";
+
+    /// <summary>
+    /// PrismJS theme file for the Tomorrow theme
+    /// <code>_content/Cameek.W3CssJs.PrismJs/css/prism-tomorrow.min.css</code>
+    /// </summary>
+    public static string PrismThemeTomorrowMinCss => "_content/Cameek.W3CssJs.PrismJs/css/prism-tomorrow.min.css";
+
+    /// <summary>
+    /// PrismJS theme file for the Twilight theme
+    /// <code>_content/Cameek.W3CssJs.PrismJs/css/prism-twilight.min.css</code>
+    /// </summary>
+    public static string PrismThemeTwilightMinCss => "_content/Cameek.W3CssJs.PrismJs/css/prism-twilight.min.css";
+
+    /// <summary>
+    /// License file for PrismJS
+    /// <code>_content/Cameek.W3CssJs.PrismJs/LICENSE-prismjs.txt</code>
+    /// </summary>
+    public static string PrismLicense => "_content/Cameek.W3CssJs.PrismJs/LICENSE-prismjs.txt";
 }
